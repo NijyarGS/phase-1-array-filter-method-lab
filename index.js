c3e88985b71dd3b4dd85f67c4d6828b9330dee10
Nijyar Ghazi <nijyar360@gmail.com>
2022-09-19 17:08:27 +0300

    completed assigment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+function findMatching(listOfNames,name){
+
+    return listOfNames.filter(e => e.toLowerCase()==name.toLowerCase())
+
+}
+function fuzzyMatch(listOfNames,name) {
+
+    return listOfNames.filter(e=> e.charAt(0) == name.charAt(0))
+}
+
+
+function matchName(listOfNames,name){
+
+    return listOfNames.filter(e=> e.name == name)
+}
